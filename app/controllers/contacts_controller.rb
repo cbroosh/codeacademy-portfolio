c8ccaf4b2a60dd228a9d0bf48369e190595c2ada
chris brucher <c.p.brucher@gmail.com>
2015-08-05 01:23:00 +0000

    Added contact email code to Contacts Controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,10 +2,18 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.new(contact_params)
         if @contact.save
+            flash[:success] = 'Message sent.'
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
+            
+            ContactMailer.contact_email(name, email, body).deliver
             redirect_to new_contact_path, notice: "Message sent."
         else
             redirect_to new_contact_path, notice: "Something went wrong."
         end
+        
+        
     end
     
     def new
